b0399804d89766208c519ef40a1e15c814cf0096
unknown <rayavarapunagasuryakiran@gmail.com>
2025-06-11 12:03:16 +0530

    initial commit

--- factorial.py
+++ factorial.py
@@ -6,7 +6,17 @@ def fact (n):
         return n* fact(n-1)
 n=5
 print(fact(n))
-#single line code for fatorial
-1 if n==0 or n==1 else n*fact(n-1)
+#single line code for fatorial (note disable above code=dacadd)
+def fact(n):
+    return 1 if (n==0 or n==1) else n*fact(n-1)
 n=6
 print (fact(n))
+#using math function
+import math
+def facts(d):
+    if d<0:
+        return None
+    else:
+        return math.factorial(d)
+d=-1
+print(facts(d))
