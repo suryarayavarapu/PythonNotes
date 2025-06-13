9cf0cd5f5c9aa842b39c86c74002a30bd5f692b5
unknown <rayavarapunagasuryakiran@gmail.com>
2025-06-13 16:56:00 +0530

    check value in fibnoic sequence

---
+++ check_fib.py
@@ -0,0 +1,8 @@
+fib={}
+fib[0]=0
+fib[1]=1
+for i in range (2,10):
+    fib[i]=fib[i-1]+fib[i-2]
+if int(22) in fib.values():
+    print ("fibnoic value present")
+else:print ("fibnoic value not present")
