4cbece20713ba113144627315e83fe91b8c780a2
unknown <rayavarapunagasuryakiran@gmail.com>
2025-06-16 18:01:09 +0530

    asci value of character

---
+++ asci_char.py
@@ -0,0 +1,2 @@
+var=input("enter character to fingd its ascii value")
+print("asci value of charactr'"+  var+ "'is " ,ord(var))
--- check_fib.py
+++ check_fib.py
@@ -5,4 +5,4 @@ for i in range (2,10):
     fib[i]=fib[i-1]+fib[i-2]
 if int(22) in fib.values():
     print ("fibnoic value present")
-else:print ("fibnoic value not present")
+else:print ("fibnoic value not  present")
