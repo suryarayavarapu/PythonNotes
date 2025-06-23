b299c7e30ce6294a1a0595de3b1dbc0efcbe5fa0
unknown <rayavarapunagasuryakiran@gmail.com>
2025-06-23 22:19:17 +0530

    for looo in data types

---
+++ for_loop_dataTypes.py
@@ -0,0 +1,18 @@
+#for loopn with data types
+#printing each charachter in new line in a string
+name = "naga durya kiran"
+for c in name:
+    print (c)
+# i need all characters i single line
+fame="hello world"
+for j in fame:
+    print(j,end=" ")
+print ("\n")
+#reverse of a string using for loop
+content= "harikrishna"
+for i in content[::-1]:
+    print(i,end= ' ')
+#without using for loop based on syntax
+message="welcome"
+print(message[::-1])
+  
