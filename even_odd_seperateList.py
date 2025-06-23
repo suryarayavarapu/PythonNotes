a668f67f88be662f3dc8e94e9d029b0905306780
unknown <rayavarapunagasuryakiran@gmail.com>
2025-06-24 03:02:09 +0530

    seperateLISTfor even& odd num

---
+++ even_odd_seperateList.py
@@ -0,0 +1,21 @@
+j=[]
+#for i in range(1,100):
+#   if i%2==0:
+#       print(i,end=" ")
+#print("\n\n")
+#for k in range(1,100):
+#    if k%2!=0:
+#        print(k,end=" ")
+numbers=list(range(100,160))
+#lis2 even numbers, list3 odd numbers
+list2=[]
+list3=[]
+print(numbers)
+for number in numbers:
+    if (number %2 )==0:
+        list2.append(number)
+    else:
+        list3.append(number)
+#while n<range(len(100,140)):
+print(list2)
+print(list3)
