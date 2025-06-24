bee1f20824dc62544a2406e513b3fc6fe34e4212
unknown <rayavarapunagasuryakiran@gmail.com>
2025-06-24 05:57:52 +0530

    rightpyramidpattern

---
+++ right_pyramid_stars.py
@@ -0,0 +1,10 @@
+for i in range(1,6):
+    for j in range(1,6):
+        if j<=(i):
+            print ("*",end= " ")
+    print("\n")
+for k in range(5,0,-1):
+    for l in range(1,5,1):
+        if l<(k):
+            print("*",end=" ")
+    print("\n")
