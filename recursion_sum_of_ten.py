84d33d6d3f103da2f556e388f164fcf3da3ecefa
unknown <rayavarapunagasuryakiran@gmail.com>
2025-07-16 03:57:58 +0530

    recursion using functions

--- recursion_sum_of_ten.py
+++ recursion_sum_of_ten.py
@@ -7,7 +7,25 @@ def recursion(x):
         return x
 print(recursion(9))
 print("\n")
-#
+#if key value pair passing arguments "**" required 
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+my_function(fname = "Tobias", lname = "Refsnes")
+print("\n\n\n")
+#if not key value pair single "*" enough
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+my_function("Emil", "Tobias", "Linus")
+print("\n\n\n")
+#Passing a List as an Argument
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+#recursion sum of ten numbers in recursion mode
 def add_first(x):
     if (0<x<11):
         x+=add_first((x-1))
@@ -18,5 +36,4 @@ def add_first(x):
     else:
         print("its not first 10 numbers sum")
 add_first(-1)
-#using recursion
 
