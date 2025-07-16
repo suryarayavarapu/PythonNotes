dc3a3769d6bd9462f7df91b783f8f0d4a16619fe
unknown <rayavarapunagasuryakiran@gmail.com>
2025-07-16 09:49:13 +0530

    inheritance OOPS

--- inhertinace.py
+++ inhertinace.py
@@ -6,11 +6,16 @@ class Employee():
     def __stri__(self):
         return f"my name is {self.name} and age is {self.age} ,received salary{self.salary}"
     def mymanager(self):
-        return f"my manager is {self.name}"
+        return f"I am employee and my name is {self.name}"
 class Manager(Employee):
     def mymanager(self):
         return f"my manager is {self.name}"
+class Hr(Employee):
+    def hr(self):
+        return f"Surya(The manager) gave per month salary to {self.name} around {self.salary}/- Rupees because she deserved it"
 x1=Employee("hari",17,"1lakh")
 print(x1.mymanager())
 x2=Manager("surya",22,"2lakhs")
 print(x2.mymanager())
+x3=Hr("kruthi",25,"3.9 Lakh")
+print(x3.hr())
