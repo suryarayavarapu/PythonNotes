b50ee4705eb3cd45935eadfa5336074d707732de
unknown <rayavarapunagasuryakiran@gmail.com>
2025-07-16 09:50:27 +0530

    iterators inclding ooops

---
+++ iterators.py
@@ -0,0 +1,26 @@
+string="banana"
+for i in string:
+    print(i)
+string1=["abc","def","ghi"]
+for string in string1:
+    print(string)
+string3=("a","vf","aerw")
+for string in string3:
+    print(string)
+string5="hercules"
+do=iter(string5)
+print(next(do))
+print(next(do))
+print(next(do))
+print(next(do))
+print(next(do))
+print(next(do))
+print(next(do))
+print(next(do))
+strings=("bha","vani","sury","hai","csd")
+need=iter(strings)
+print(next(need))
+print(next(need))
+print(next(need))
+print(next(need))
+print(next(need))
